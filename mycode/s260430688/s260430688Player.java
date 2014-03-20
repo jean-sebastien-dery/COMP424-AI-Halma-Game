db1c2004ac04b48aaad06f89866f030e65171c15
jdery <jdery@jdery-S400CA.(none)>
2014-03-20 16:55:46 -0400

    Added my code

---
+++ mycode/s260430688/s260430688Player.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package s260430688;
+
+import boardgame.Board;
+import boardgame.Move;
+import boardgame.Player;
+
+/**
+ * @author Jean-Sebastien Dery (260 430 688)
+ *
+ */
+public class s260430688Player extends Player {
+
+	public s260430688Player(String name) {
+		super("s260430688");
+	}
+
+	@Override
+	public Move chooseMove(Board board) {
+		
+		return null;
+	}
+}
