b535fbd02dcf3be31cde4bd698734e9726f078a2
Jean-Sebastien Dery <jean-sebastien.dery@mail.mcgill.ca>
2014-04-29 10:00:01 -0400

    Cleaned up the repository

--- ultimate.sh
+++ ultimate.sh
@@ -7,8 +7,11 @@ gnome-terminal --title="Server" -x java -cp ./jar/projectsrc.jar boardgame.Serve
 echo "Starting the first player.";
 gnome-terminal --title="Player 0" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.s260430688Player localhost 8123
 echo "Starting the second player.";
-gnome-terminal --title="Player 1" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.myPlayer1 localhost 8123
+gnome-terminal --title="Player 0" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.s260430688Player localhost 8123
+#gnome-terminal --title="Player 1" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.myPlayer1 localhost 8123
 echo "Starting the third player.";
-gnome-terminal --title="Player 2" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.myPlayer2 localhost 8123
+gnome-terminal --title="Player 0" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.s260430688Player localhost 8123
+#gnome-terminal --title="Player 2" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.myPlayer2 localhost 8123
 echo "Starting the fourth player.";
-gnome-terminal --title="Player 3" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.myPlayer3 localhost 8123
+gnome-terminal --title="Player 0" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.s260430688Player localhost 8123
+#gnome-terminal --title="Player 3" -x java -cp ./jar/projectsrc.jar:/home/jdery/workspace/PrjAIProject/bin/ boardgame.Client s260430688.myPlayer3 localhost 8123
